8df40d7c196d6ed11bb04e6c6a96588aad0a2812
Begad <64358501+begadsalah@users.noreply.github.com>
2021-11-21 12:20:30 +0200

    Updates

---
+++ precache-manifest.95890aa8e97f8b78042fa14cb91916fc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf7ebf2e8c4a3cb8632b8b6fee11ac0",
+    "url": "/carlos/index.html"
+  },
+  {
+    "revision": "570caf74a945f3558679",
+    "url": "/carlos/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "e44a0018f2eba8141fc5",
+    "url": "/carlos/static/css/main.13ce3300.chunk.css"
+  },
+  {
+    "revision": "570caf74a945f3558679",
+    "url": "/carlos/static/js/2.73fa9107.chunk.js"
+  },
+  {
+    "revision": "67364e4f8a92ee3414e0588d01fa6997",
+    "url": "/carlos/static/js/2.73fa9107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44a0018f2eba8141fc5",
+    "url": "/carlos/static/js/main.9c22ba5b.chunk.js"
+  },
+  {
+    "revision": "e9b1e3f29ee13d7778a8",
+    "url": "/carlos/static/js/runtime-main.d6148445.js"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/carlos/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/carlos/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/carlos/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/carlos/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/carlos/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/carlos/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/carlos/static/media/slickd41d.ced611da.eot"
+  }
+]);
